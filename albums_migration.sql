e33cb5f5797c3a31acdeb00f52a2ecb2f5992dbc
Presley Black <presleyblack24@gmail.com>
2023-08-17 11:57:55 -0500

    first mysql exersise

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'UNKNOWN',
+    name VARCHAR(75) DEFAULT 'UNKNOWN',
+    release_date INT,
+    sales FLOAT UNSIGNED,
+    genre VARCHAR(30) DEFAULT 'UNKNOWN',
+    PRIMARY KEY(id)
+);
