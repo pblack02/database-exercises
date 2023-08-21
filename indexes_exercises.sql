be9c497ba5abee4d8437a83a45a027e422af9740
Presley Black <presleyblack24@gmail.com>
2023-08-21 16:41:12 -0500

    finished join exercises

---
+++ indexes_exercises.sql
@@ -0,0 +1,9 @@
+USE employees;
+DESCRIBE employees;
+
+USE codeup_test_db;
+DESCRIBE albums;
+
+ALTER TABLE albums
+ADD UNIQUE (name, artist);
+
---
+++ join_exercises.sql
@@ -0,0 +1,54 @@
+USE employees;
+SELECT *
+FROM employees;
+SELECT *
+FROM departments;
+SELECT *
+FROM dept_manager;
+
+SELECT DISTINCT title FROM titles;
+
+
+SELECT d.dept_name AS department, CONCAT(e.last_name, ' ', e.first_name) AS manger
+FROM employees AS e
+    JOIN dept_manager AS dm
+        on e.emp_no = dm.emp_no
+    JOIN departments AS d
+        on dm.dept_no = d.dept_no
+WHERE YEAR(to_date) = 9999;
+
+
+SELECT d.dept_name AS department, CONCAT(e.last_name, ' ', e.first_name) AS manger, gender
+FROM employees AS e
+         JOIN dept_manager AS dm
+              on e.emp_no = dm.emp_no
+         JOIN departments AS d
+              on dm.dept_no = d.dept_no
+
+WHERE YEAR(to_date) = 9999 AND gender = 'F';
+
+
+SELECT titles.title, COUNT(*)
+FROM titles
+JOIN dept_emp
+    on titles.emp_no = employees.dept_emp.emp_no
+JOIN departments
+    on departments.dept_no = employees.dept_emp.dept_no
+WHERE (dept_name = 'Customer Service'
+    AND YEAR(dept_emp.to_date) = 9999
+    AND YEAR(titles.to_date) = 9999)
+GROUP BY titles.title;
+
+SELECT d.dept_name AS department, CONCAT(e.first_name, ' ',e.last_name) AS manger, salary
+FROM employees AS e
+         JOIN dept_manager AS dm
+              on e.emp_no = dm.emp_no
+         JOIN departments AS d
+              on dm.dept_no = d.dept_no
+         JOIN salaries AS s
+              on e.emp_no = s.emp_no
+WHERE (YEAR(s.to_date) = 9999 AND YEAR(dm.to_date) = 9999);
+
+
+
+
